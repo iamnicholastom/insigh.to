fde2500a08e689601b2569b408ed7ebbaa75c8a1
Nicholas Tom <ntompg@gmail.com>
2024-12-18 23:55:18 +0530

    minor variable changes

--- auth.js
+++ auth.js
@@ -12,8 +12,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       name: "email",
     }),
     Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
